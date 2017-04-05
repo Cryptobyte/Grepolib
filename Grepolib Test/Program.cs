bcc7c3949e55f94223d09d6519788c299c02f6ce
Cryptobyte <cryptobyte1134@gmail.com>
2017-04-05 14:21:15 -0400

    Updated Program.cs

--- Grepolib Test/Program.cs
+++ Grepolib Test/Program.cs
@@ -6,24 +6,55 @@ namespace Grepolib_Test
 {
     class Program
     {
+        static void updateStatus(World world, string status = null)
+        {
+            if (status == null)
+            {
+                Console.Title = "Grepolib | " + world.Region.ToString() + world.WorldID.ToString();
+                return;
+            }
+
+            Console.Title = "Grepolib | " + world.Region.ToString() + world.WorldID.ToString() + " | " + status;
+        }
+
         static void Main(string[] args)
         {
+            Console.Title = "Grepolib";
+
+            // Test Value = EN
+            Console.Write("Enter Region: ");
+            Region region = (Region)Enum.Parse(typeof(Region), Console.ReadLine(), true);
+
+            // Test Value = 96
+            Console.Write("Enter World Number: ");
+            int wid = int.TryParse(Console.ReadLine(), out wid) ? wid : 0;
+
+            Console.Clear();
+
+            World world = new World(wid, region);
+
+            updateStatus(world, "Fetching Players..");
             Console.Write("Fetching Player List..");
-            List<Player> players = new World(96, Region.EN).Players;
+            List<Player> players = world.Players;
             Console.Write(" Done! [{0}]\n", players.Count);
 
-            Console.WriteLine("Fetching Alliance List..");
-            List<Alliance> alliances = new World(96, Region.EN).Alliances;
+            updateStatus(world, "Fetching Alliances..");
+            Console.Write("Fetching Alliance List..");
+            List<Alliance> alliances = world.Alliances;
             Console.Write(" Done! [{0}]\n", alliances.Count);
 
-            Console.WriteLine("Fetching Town List..");
-            List<Town> towns = new World(96, Region.EN).Towns;
+            updateStatus(world, "Fetching Towns..");
+            Console.Write("Fetching Town List..");
+            List<Town> towns = world.Towns;
             Console.Write(" Done! [{0}]\n", towns.Count);
 
-            Console.WriteLine("Fetching Island List..");
-            List<Island> islands = new World(96, Region.EN).Islands;
+            updateStatus(world, "Fetching Islands..");
+            Console.Write("Fetching Island List..");
+            List<Island> islands = world.Islands;
             Console.Write(" Done! [{0}]\n", islands.Count);
-            
+
+            Console.Clear();
+            updateStatus(world, "Ready");
             Console.ReadLine();
         }
     }
